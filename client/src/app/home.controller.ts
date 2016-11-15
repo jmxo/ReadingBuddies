9179415e31ca6d704efa17ce31c535fd38dd2447
Mohamed Ismat Abdelhameed <mohamedismatlinkedin@gmail.com>
2016-11-15 19:50:03 +0300

    converted homeController to ts

--- client/src/app/home.controller.ts
+++ client/src/app/home.controller.ts
@@ -1,16 +1,18 @@
-(function () {
+namespace app {
     'use strict';
+
+    /**
+    * Home view controller
+    */
+    class HomeController {
+        public storage: ng.storage.IStorageService;
+
+        constructor($localStorage: ng.storage.IStorageService) {
+            this.storage = $localStorage;
+        }
+    }
 
     angular
         .module('app')
         .controller('HomeController', HomeController);
-
-    /**
-     * Home view controller
-     */
-    // @ngInject
-    function HomeController($localStorage) {
-        var vm = this;
-        vm.storage = $localStorage;
-    }
-})();
+}
